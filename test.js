ec659f2d16af8d4f34111bbd4b335e0d2052f55d
Ali <mythologyali@gmail.com>
2018-02-15 04:19:13 -0800

    removed test

--- test.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function testFunction(message) {
-    message.channel.send('hello');
-}
-exports.testFunction = testFunction;
--- test.ts
+++
@@ -1,7 +0,0 @@
-import * as Discord from 'discord.js'
-
-function testFunction(message: Discord.Message){
-    message.channel.send('hello');
-}
-
-export {testFunction}
