d828ad31ea0597a6d2d3184b858792b2f4bc4ae7
Ali <mythologyali@gmail.com>
2018-02-14 20:18:01 -0800

    ok relax I didn't remove alexa

--- Moderation/ScheduleUnmute.js
+++ Moderation/ScheduleUnmute.js
@@ -1,12 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Logging_1 = require("./../Logging");
 function scheduleUnmute(member, time) {
+    var mutedMemberName = member.nickname || member.user.username;
     var muted = member.guild.roles.find('name', 'muted');
-    if (!muted)
-        return Logging_1.debug.warning("Could not schedule unmute for user " + member.user.username + ", missing 'muted' role.");
-    setTimeout(function () {
-        member.removeRole(muted);
-    }, time);
 }
 exports.scheduleUnmute = scheduleUnmute;
--- Moderation/ScheduleUnmute.ts
+++ Moderation/ScheduleUnmute.ts
@@ -2,11 +2,9 @@ import * as Discord from 'discord.js'
 import {debug}from './../Logging'
 
 export function scheduleUnmute(member : Discord.GuildMember, time: number){
+    const mutedMemberName : string = member.nickname || member.user.username;
     const muted : any = member.guild.roles.find('name', 'muted');
-    if (!muted)
-        return debug.warning(`Could not schedule unmute for user ${member.user.username}, missing 'muted' role.`);
 
-    setTimeout(function(){
-        member.removeRole(muted);
-    }, time);
+
+
 }
