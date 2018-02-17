19973a81e5b30c4536465b331438cbe5d3487d8b
Ali <mythologyali@gmail.com>
2018-02-17 04:13:51 -0800

    regex

--- Listeners/Regex.ts
+++ Listeners/Regex.ts
@@ -1,1 +1,1 @@
-const youtubeInviteRegex : RegExp = /((https?):\/\/(www\.)?)?youtu\.?be(\.com)?\/(watch\?v=)?\w{11}/;
+export const youtubeInviteRegex : RegExp = /((https?):\/\/(www\.)?)?youtu\.?be(\.com)?\/(watch\?v=)?\w{11}/;
