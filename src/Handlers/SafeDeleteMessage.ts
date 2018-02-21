bba26d2f2c6f22f642492c418b1f7f546a1e5642
Ali <mythologyali@gmail.com>
2018-02-21 01:50:55 -0800

    module for safely deleting user messages

---
+++ src/Handlers/SafeDeleteMessage.ts
@@ -0,0 +1,33 @@
+import {DiscordAPIError, Message} from "discord.js";
+import * as dbg from 'debug'
+import {APIErrors} from "../Interfaces/Errors";
+
+const debug = {
+    info: dbg('Bot:DeleteMessage:Info'),
+    warning: dbg('Bot:DeleteMessage:Warning'),
+    error: dbg('Bot:DeleteMessage:Error')
+};
+
+export default function safeDeleteMessage(message : Message) : Promise<void>{
+    return new Promise<void>(function (resolve) {
+
+        message.delete().then(() => {
+            resolve();
+        }).catch(error => {
+            if (error instanceof DiscordAPIError){
+                if (error.message === APIErrors.MISSING_PERMISSIONS){
+                    debug.error(`Could not delete message from ${message.author.username}`+
+                        ` in ${message.guild.name}, missing permissions.`);
+                }
+                else {
+                    debug.error(error);
+                }
+            }
+            else {
+                debug.error(`Unexpected error while trying to delete message from `+
+                    `${message.author.username} in ${message.guild.name}.`, error);
+            }
+            // we don't want to reject here because we're already handling everything here
+        })
+    });
+}
