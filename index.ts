39c7630ed7972fe654ede5e7a2789fc7f61b5e8b
Ali <mythologyali@gmail.com>
2018-02-21 11:02:16 -0800

    heroku startup

--- index.ts
+++ index.ts
@@ -1,8 +1,25 @@
+
+declare let process : {
+    env: {
+        BOT_TOKEN: string,
+        CLEVERBOT_TOKEN: string
+    }
+};
+
+let BOT_TOKEN : string;
+let CLEVERBOT_TOKEN : string;
+
+if (process.env.BOT_TOKEN !== undefined && process.env.CLEVERBOT_TOKEN !== undefined){
+    BOT_TOKEN = process.env.BOT_TOKEN;
+    CLEVERBOT_TOKEN = process.env.CLEVERBOT_TOKEN;
+} else {
+    BOT_TOKEN = require('./config0.json').TOKEN;
+    CLEVERBOT_TOKEN = require('./config0.json').CleverBotAPI;
+}
+
 // modules
 import {Alexa} from "./src/API/Alexa";
 import "reflect-metadata";
-const config = require('./config0.json');
-const cleverbotAPI = require('./config0.json').CleverBotAPI;
 import {MuteQueue} from './src/Moderation/MuteQueue'
 import {MessageQueue} from "./src/Moderation/MessageQueue";
 import {debug} from './src/Utility/Logging'
@@ -14,11 +31,11 @@ import * as Discord from 'discord.js'
 
 // instances
 const bot          : Discord.Client = new Discord.Client();
-const alexa        : Alexa          = new Alexa(cleverbotAPI);
+const alexa        : Alexa          = new Alexa(CLEVERBOT_TOKEN);
 const muteQueue    : MuteQueue      = new MuteQueue();
 const messageQueue : MessageQueue   = new MessageQueue(muteQueue);
 
-bot.login(config.TOKEN);
+bot.login(BOT_TOKEN);
 
 bot.on('ready', function(){
     onReady(bot);
