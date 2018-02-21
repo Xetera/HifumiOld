0418234e7ec2f5bdebb43d499acb0609a51709c1
Ali <mythologyali@gmail.com>
2018-02-21 01:52:18 -0800

    updated index and entry point

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 // modules
-var config = require('./refactor.json');
+var Alexa_1 = require("./src/API/Alexa");
+var config = require('./config0.json');
+var cleverbotAPI = require('./config0.json').CleverBotAPI;
+var MuteQueue_1 = require("./src/Moderation/MuteQueue");
+var MessageQueue_1 = require("./src/Moderation/MessageQueue");
 var onReady_1 = require("./src/Events/onReady");
+var onMessage_1 = require("./src/Events/onMessage");
 // dependencies
 var Discord = require("discord.js");
+// instances
 var bot = new Discord.Client();
-bot.login(config.token);
+var alexa = new Alexa_1.Alexa(cleverbotAPI);
+var muteQueue = new MuteQueue_1.MuteQueue();
+var messageQueue = new MessageQueue_1.MessageQueue(muteQueue);
+bot.login(config.TOKEN);
 bot.on('ready', function () {
     onReady_1.default(bot);
+});
+bot.on('message', function (message) {
+    onMessage_1.default(message, alexa, messageQueue, bot);
 });
--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 // modules
 import {Alexa} from "./src/API/Alexa";
 
-const config = require('./refactor.json');
-const cleverbotAPI = require('./config0.json').CleverbotAPI;
+const config = require('./config0.json');
+const cleverbotAPI = require('./config0.json').CleverBotAPI;
 import {MuteQueue} from './src/Moderation/MuteQueue'
 import {MessageQueue} from "./src/Moderation/MessageQueue";
 import {debug} from './src/Utility/Logging'
@@ -18,12 +18,12 @@ const alexa        : Alexa          = new Alexa(cleverbotAPI);
 const muteQueue    : MuteQueue      = new MuteQueue();
 const messageQueue : MessageQueue   = new MessageQueue(muteQueue);
 
-bot.login(config.token);
+bot.login(config.TOKEN);
 
 bot.on('ready', function(){
     onReady(bot);
 });
 
 bot.on('message', function(message){
-    onMessage(message, alexa, messageQueue);
+    onMessage(message, alexa, messageQueue, bot);
 });
