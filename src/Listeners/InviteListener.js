9b1b372293fb73de54e3ca478545adb818fee6a5
Ali <mythologyali@gmail.com>
2018-02-21 01:51:23 -0800

    inviteListener module implemented

---
+++ src/Listeners/InviteListener.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Regex_1 = require("./Regex");
+var onMessage_1 = require("../Events/onMessage");
+var Settings_1 = require("../Utility/Settings");
+var SafeDeleteMessage_1 = require("../Handlers/SafeDeleteMessage");
+var SafeMessageUser_1 = require("../Handlers/SafeMessageUser");
+var Replies_1 = require("../Moderation/Replies");
+function inviteListener(message) {
+    var sender = message.member.nickname || message.author.username;
+    if (message.content.match(Regex_1.discordInviteRegex)) {
+        onMessage_1.debug.warning(sender + " in " + message.guild + " sent an invite link.");
+        if (Settings_1.securityLevel === Settings_1.SecurityLevels.Dangerous)
+            return;
+        SafeDeleteMessage_1.default(message).then(function () {
+            onMessage_1.debug.info("Deleted invite link from " + sender);
+            SafeMessageUser_1.default(message.author, Replies_1.bannedForSpammingInvites(message.guild), "spamming");
+        });
+    }
+}
+exports.default = inviteListener;
---
+++ src/Listeners/InviteListener.ts
@@ -0,0 +1,22 @@
+import {Message} from 'discord.js'
+import {discordInviteRegex} from "./Regex";
+import {debug} from '../Events/onMessage'
+import {securityLevel, SecurityLevels} from "../Utility/Settings";
+import safeDeleteMessage from "../Handlers/SafeDeleteMessage";
+import safeMessageUser from "../Handlers/SafeMessageUser";
+import {bannedForSpammingInvites} from "../Moderation/Replies";
+
+
+export default function inviteListener(message : Message){
+    const sender :string = message.member.nickname || message.author.username;
+    if (message.content.match(discordInviteRegex)){
+        debug.warning(`${sender} in ${message.guild} sent an invite link.`);
+
+        if (securityLevel === SecurityLevels.Dangerous) return;
+        safeDeleteMessage(message).then(()=> {
+            debug.info(`Deleted invite link from ${sender}`);
+            safeMessageUser(message.author, bannedForSpammingInvites(message.guild), `spamming`);
+        });
+    }
+}
+
