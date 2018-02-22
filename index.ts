0fcc76cd8a69c43f2feb7380051b8e61fc0cbc64
Ali <mythologyali@gmail.com>
2018-02-21 23:04:57 -0800

    index.ts heroku integration

--- index.ts
+++ index.ts
@@ -1,8 +1,28 @@
+import onGuildMemberRemove from "./src/Events/onGuildMemberRemove";
+
+declare let process : {
+    env: {
+        BOT_TOKEN: string,
+        CLEVERBOT_TOKEN: string
+    }
+};
+
+let BOT_TOKEN : string;
+let CLEVERBOT_TOKEN : string;
+
+if (process.env.BOT_TOKEN !== undefined && process.env.CLEVERBOT_TOKEN !== undefined){
+    // settings for heroku
+    BOT_TOKEN = process.env.BOT_TOKEN;
+    CLEVERBOT_TOKEN = process.env.CLEVERBOT_TOKEN;
+} else {
+    // settings for development
+    BOT_TOKEN = require('./config0.json').TOKEN;
+    CLEVERBOT_TOKEN = require('./config0.json').CleverBotAPI;
+}
+
 // modules
 import {Alexa} from "./src/API/Alexa";
 import "reflect-metadata";
-const config = require('./config0.json');
-const cleverbotAPI = require('./config0.json').CleverBotAPI;
 import {MuteQueue} from './src/Moderation/MuteQueue'
 import {MessageQueue} from "./src/Moderation/MessageQueue";
 import {debug} from './src/Utility/Logging'
@@ -10,20 +30,29 @@ import onReady from './src/Events/onReady'
 import onMessage from './src/Events/onMessage'
 // dependencies
 import * as Discord from 'discord.js'
+import onGuildMemberAdd from "./src/Events/onGuildMemberAdd";
 
 
 // instances
 const bot          : Discord.Client = new Discord.Client();
-const alexa        : Alexa          = new Alexa(cleverbotAPI);
+const alexa        : Alexa          = new Alexa(CLEVERBOT_TOKEN);
 const muteQueue    : MuteQueue      = new MuteQueue();
 const messageQueue : MessageQueue   = new MessageQueue(muteQueue);
 
-bot.login(config.TOKEN);
+bot.login(BOT_TOKEN);
 
 bot.on('ready', function(){
     onReady(bot);
 });
 
-bot.on('message', function(message){
+bot.on('message', function(message : Discord.Message){
     onMessage(message, alexa, messageQueue, bot);
+});
+
+bot.on('guildMemberAdd', function(member : Discord.GuildMember){
+    onGuildMemberAdd(member);
+});
+
+bot.on('guildMemberRemove', function(member : Discord.GuildMember){
+    onGuildMemberRemove(member);
 });
