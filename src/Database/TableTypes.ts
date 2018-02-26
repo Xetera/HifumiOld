e1972ec3921ab47f4197061db087d7247fd5833d
Ali <mythologyali@gmail.com>
2018-02-26 05:37:08 -0800

    added interface guards for postgres types

--- src/Database/TableTypes.ts
+++ src/Database/TableTypes.ts
@@ -3,6 +3,7 @@ export interface IGuild {
     name: string;
     prefix: string;
     allowsinvites: boolean;
+    default_channel: string;
 }
 
 export interface IUser {
@@ -20,4 +21,21 @@ export interface IBlacklistedLinks {
 export interface IWhitelistedInvites {
     guild_id: string;
     link: string;
+}
+
+
+export function isIGuild(object : any) : object is IGuild {
+    return 'id' in object;
+}
+
+export function isIUser(object : any): object is IUser{
+    return 'guild_id' in object;
+}
+
+export function isIBlacklistedLinks(object : any): object is IBlacklistedLinks{
+    return 'guild_id' in object;
+}
+
+export function isIWhitelistedInvites(object : any): object is IWhitelistedInvites{
+    return 'guild_id' in object;
 }
