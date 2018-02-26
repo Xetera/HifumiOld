9b2388554aa4aaf88561f8a3a3739feee85f3e75
Ali <mythologyali@gmail.com>
2018-02-26 05:35:08 -0800

    system startup file for checking env variables

---
+++ src/Events/systemStartup.ts
@@ -0,0 +1,80 @@
+import * as dbg from "debug";
+import {PostgresLiveLoginConfig, PostgresDevLoginConfig} from "../Database/Database";
+
+export const debug = {
+    silly  : dbg('Bot:SystemStartup:Silly'),
+    info   : dbg('Bot:SystemStartup:Info'),
+    warning: dbg('Bot:SystemStartup:Warning'),
+    error  : dbg('Bot:SystemStartup:Error')
+};
+
+export enum Environments {
+    Development,
+    Live
+}
+
+declare let process : {
+    env: {
+        BOT_TOKEN: string,
+        CLEVERBOT_TOKEN: string
+        ENV: string;
+        DATABASE_URL: string;
+    }
+};
+
+export function getEnvironmentSettings() : Environments{
+    let env;
+
+    if (process.env.ENV === "LIVE"){
+        debug.info('Current environment is Live.');
+        env = Environments.Live;
+    }
+    else if (process.env.ENV === "DEVELOPMENT"){
+        debug.info('Current environment is Development');
+        env = Environments.Development;
+    }
+    else {
+        debug.error(`Unexpected environment: ${process.env.ENV}, setting environment to DEVELOPMENT.`);
+        env = Environments.Development;
+    }
+    return env;
+}
+
+export function getTokens(env: Environments) {
+    let BOT_TOKEN : string;
+    let CLEVERBOT_TOKEN : string;
+
+    if (env === Environments.Live){
+        // settings for heroku
+        BOT_TOKEN = process.env.BOT_TOKEN;
+        CLEVERBOT_TOKEN = process.env.CLEVERBOT_TOKEN;
+    } else if (env === Environments.Development){
+        // settings for development
+        BOT_TOKEN = require('../../config0.json').TOKEN;
+        CLEVERBOT_TOKEN = require('../../config0.json').CleverBotAPI;
+    }
+    else {
+        debug.error(`Unexpected environment: ${env}, setting token variables assuming deployment.`);
+        BOT_TOKEN = require('../../config0.json').TOKEN;
+        CLEVERBOT_TOKEN = require('../../config0.json').CleverBotAPI;
+    }
+    return [BOT_TOKEN, CLEVERBOT_TOKEN];
+}
+
+
+export function getDatabaseConnection(env: Environments) : PostgresDevLoginConfig|PostgresLiveLoginConfig {
+    if (env === Environments.Live){
+        let credentials = <PostgresLiveLoginConfig>{};
+        credentials.connectionString = process.env.DATABASE_URL;
+        credentials.ssl = true;
+        return credentials;
+    }
+    else {
+        let credentials = <PostgresDevLoginConfig>{};
+        credentials.user = 'postgres';
+        credentials.host = 'localhost';
+        credentials.port = 5432;
+        credentials.database = 'discord';
+        return credentials;
+    }
+}
