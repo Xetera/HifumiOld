86e51b398d0caae393506cef3dc6371d2f872f4d
Ali <mythologyali@gmail.com>
2018-02-28 03:11:49 -0800

    setprefix only allowed on admins

--- src/Commands/Utilty/SetPrefix.ts
+++ src/Commands/Utilty/SetPrefix.ts
@@ -2,6 +2,7 @@ import * as Discord from 'discord.js'
 import {IGuild, isIGuild} from "../../Database/TableTypes";
 import {Database} from "../../Database/Database";
 import * as dbg from "debug";
+import {adminOnlyCommand} from "../../Handlers/Replies";
 
 export const debug = {
     error  : dbg('Bot:setPrefix:Error')
@@ -9,7 +10,8 @@ export const debug = {
 
 export default function setPrefix(message: Discord.Message, prefix: string, database: Database){
     if (prefix === undefined) return message.channel.send('No prefix was entered.');
-
+    else if (!message.member.hasPermission('ADMINISTRATOR'))
+        return message.channel.send(adminOnlyCommand);
     database.setPrefix(message.guild, prefix).then((res:IGuild|Error|-1 )=> {
         if (isIGuild(res))
             message.channel.send('Prefix changed to ' + res.prefix);
--- src/Handlers/Replies.ts
+++ src/Handlers/Replies.ts
@@ -1,9 +1,12 @@
 import {Guild} from "discord.js";
 
+export const adminOnlyCommand : string = 'This command is only available to admins.';
+
 export function bannedForSpammingInvites(guild : Guild) : string {
     return `I went ahead and banned you for spamming invites in `+
     `'${guild.name}', advertising isn't allowed, sorry.`;
 }
+
 export const welcomeMessages = [
     'You must be new around here kid...',
     "Interesting, I haven't seen you around before.",
