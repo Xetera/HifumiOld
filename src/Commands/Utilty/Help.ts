90a3719815ecc8e327dbfee7db7b1136ede55c9a
Ali <mythologyali@gmail.com>
2018-02-27 23:55:55 -0800

    added .help command

--- src/Commands/Utilty/Help.ts
+++ src/Commands/Utilty/Help.ts
@@ -1,5 +1,32 @@
-import * as fs from 'fs'
+import * as Discord from 'discord.js'
+import './../help.json'
+import {Database} from "../../Database/Database";
+const help = require('./../Help.json');
 
-export function getHelp() {
-// stub
+export function getHelp(message : Discord.Message, args : string[],  database : Database) {
+    const prefixLine : string = `.prefix: ${database.getPrefix(message.guild.id)}\n`;
+    let lines : string = prefixLine;
+    const command = args[0];
+    let commands : string[] = [];
+
+    for (let i in help){
+        commands.push(help[i].name);
+    }
+    if (!args.length){
+        for (let i in help){
+            lines += `.${help[i].name}: ${help[i].info}\n`
+        }
+        lines += "\nuse {help (command)} to get more information about commands.";
+    }
+    else if (args.length && commands.includes(command)){
+        lines +=
+            `.${help[command].name}: ${help[command].info}\n` +
+            `.Usage: ${help[command].usage}\n` +
+            `.Example: ${help[command].example}`;
+    }
+    else {
+        return message.channel.send(args[0] + ' is not a valid command.');
+    }
+    message.channel.send('\`\`\`css\n' + lines + '\`\`\`');
+
 }
