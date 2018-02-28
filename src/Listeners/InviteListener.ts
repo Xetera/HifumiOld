7d6950486ab0d0df66bd17cde7e145b8a9a5a0d1
Ali <mythologyali@gmail.com>
2018-02-27 22:08:40 -0800

    alexa now warns users for posting invites first

--- src/Listeners/InviteListener.ts
+++ src/Listeners/InviteListener.ts
@@ -5,6 +5,7 @@ import {securityLevel, SecurityLevels} from "../Utility/Settings";
 import safeDeleteMessage from "../Handlers/SafeDeleteMessage";
 import {Database} from "../Database/Database";
 import banForInviteSpam from "../Actions/BanForInviteSpam";
+import safeMessageUser from "../Handlers/SafeMessageUser";
 
 export default function inviteListener(message : Message, database : Database){
     const sender :string = message.member.nickname || message.author.username;
@@ -16,10 +17,15 @@ export default function inviteListener(message : Message, database : Database){
             debug.info(`Deleted invite link from ${sender}`);
             return database.incrementMemberInviteStrikes(message.member)
         }).then((strikeCount : number) => {
-            debug.silly(strikeCount + " strikes on record");
+            debug.silly(`${message.member.displayName} has ` + strikeCount + " strikes on record");
             // hardcoding but we shouldn't really need to change this for anything
             if (strikeCount >= 5){
                 banForInviteSpam(message.member);
+            }
+            else if (strikeCount === 4){
+                safeMessageUser(message.member.user,
+                    `Warning: you posted 4 invites in ${message.guild.name}.` +
+                    `I don't go and advertise in your server, so please don't do it in mine.`);
             }
         })
     }
