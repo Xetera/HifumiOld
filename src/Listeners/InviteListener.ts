9d721fac963926ccc90ad3a8c7653364a85f933d
Ali <mythologyali@gmail.com>
2018-02-27 22:46:13 -0800

    alexa now doesn't remove invites from me bc I'm awesome

--- src/Listeners/InviteListener.ts
+++ src/Listeners/InviteListener.ts
@@ -6,8 +6,11 @@ import safeDeleteMessage from "../Handlers/SafeDeleteMessage";
 import {Database} from "../Database/Database";
 import banForInviteSpam from "../Actions/BanForInviteSpam";
 import safeMessageUser from "../Handlers/SafeMessageUser";
+import gb from "../Misc/Globals";
 
 export default function inviteListener(message : Message, database : Database){
+    if (message.author.id === gb.ownerID) return; // heh
+
     const sender :string = message.member.nickname || message.author.username;
     if (message.content.match(discordInviteRegex)){
         debug.warning(`${sender} in ${message.guild} sent an invite link.`);
@@ -24,10 +27,10 @@ export default function inviteListener(message : Message, database : Database){
             }
             else if (strikeCount === 4){
                 safeMessageUser(message.member.user,
-                    `Warning: you posted 4 invites in ${message.guild.name}.` +
-                    `I don't go and advertise in your server, so please don't do it in mine.`);
+                    `Warning: you've posted 4 invites in ${message.guild.name}, the next one will get you banned.` +
+                    `I don't go advertising in your server, so please don't do it in mine.`);
             }
-        })
+        });
     }
 }
 
