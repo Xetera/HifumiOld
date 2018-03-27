931c1ece24063848224963a8826c9049f832f9c0
Ali <mythologyali@gmail.com>
2018-03-27 09:00:34 -0700

    moved replies and added errors

--- src/interfaces/Replies.ts
+++ src/interfaces/Replies.ts
@@ -2,6 +2,8 @@ import {Guild} from "discord.js";
 import {random} from "../utility/Util";
 import getInvite from "../commands/DM/getInvite";
 import gb from "../misc/Globals";
+import {errors} from "pg-promise";
+import {underline} from "../utility/Markdown";
 
 export const adminOnlyCommand : string = 'This command is only available to admins.';
 
@@ -22,6 +24,10 @@ export const welcomeMessages = [
     'Why hello, what brings you here, friend?'
 ];
 
+export const onNewGuildJoin =
+    'I\'m here to help with baddies on your server and keep things nice and tidy.\n' +
+    'You can also talk to me when you\'re bored by just calling my name! That\'s how you humans normally do it... right?';
+
 export const lackingOwnerPermissions =
     '_Laughing hysterically_ oh god, I can\'t believe you thought I\'d let you do that.';
 
@@ -29,7 +35,7 @@ export function randomRuntimeError() : string {
     return random(runtimeErrorResponses);
 }
 
-export function advertiseOnRaidBan() : string {
+export function advertiseOnRaidBan(): string {
     return `Want to protect your own server from people like yourself? Invite me!\n${getInvite()}`;
 }
 
@@ -37,6 +43,5 @@ export const runtimeErrorResponses : string[] = [
     "Holy... I almost crashed while trying to do that, there was an error somewhere.",
     "I really tried my best to do that but something wen't wrong so I couldn't...",
     "This is a little bit awkward but I basically almost broke trying to do that, something's wrong.",
-    "Uh oh, something wen't wrong and I don't know what.",
-    "So I got an unexpected error trying to do that but on the bright side, I would have crashed if not for this message."
+    "Uh oh, something wen't wrong and I don't know what. Someone will look into this."
 ];
