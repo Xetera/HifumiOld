767ffa39b557c17f89f948e44c093bda2d6e4185
Ali <mythologyali@gmail.com>
2018-03-27 08:57:30 -0700

    re enabled null checking

--- src/listeners/Regex.ts
+++ src/listeners/Regex.ts
@@ -1,3 +1,5 @@
 export const youtubeInviteRegex : RegExp = /((https?):\/\/(www\.)?)?youtu\.?be(\.com)?\/(watch\?v=)?\w{11}/;
 export const discordInviteRegex : RegExp = /(\n|.)*((discord|discordapp).(gg|me|io|com\/invite)\/)(\n|.)*/;
+export const telegramInviteRegex: RegExp = /(https?):\/\/t.me\/joinchat\/[A-z 0-9 -_]+/;
+
 export const channelMentionRegex : RegExp = /\\d{18}(?=>)/;
