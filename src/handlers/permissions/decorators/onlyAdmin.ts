b04112faad571cc0fb047b335d9a0082990371ec
Ali <mythologyali@gmail.com>
2018-03-28 08:58:06 -0700

    new onlyGuildOwner permission decorator added

--- src/handlers/permissions/decorators/onlyAdmin.ts
+++ src/handlers/permissions/decorators/onlyAdmin.ts
@@ -1,13 +1,15 @@
 import {Message, PermissionString} from "discord.js";
+import gb from "../../../misc/Globals";
+import missingAdminEmbed from "../../../embeds/permissions/missingAdminEmbed";
 
 export default function onlyAdmin(target : any, key: any , descriptor: any) {
     const originalMethod = descriptor.value;
     descriptor.value = function () {
         const message : Message = arguments[0].message;
-        if (!message.member.permissions.has('ADMINISTRATOR')){
+        if (!message.member.permissions.has('ADMINISTRATOR') && message.author.id !== gb.ownerID){
             message.channel.send(
-                'Uh... sorry, I was told to only let Admins use this. '
-                + message.client.emojis.find('name', 'alexa_feels_bad_man'));
+                missingAdminEmbed()
+            );
             return;
         }
 
--- src/handlers/permissions/decorators/onlyGuildOwner.ts
+++ src/handlers/permissions/decorators/onlyGuildOwner.ts
@@ -1,13 +1,13 @@
 import * as Discord from "discord.js";
 import gb from "../../../misc/Globals";
-import missingModEmbed from "../../../embeds/permissions/missingModEmbed";
+import missingGuildOwnerEmbed from "../../../embeds/permissions/missingGuildOwnerEmbed";
 
-export default function onlyMod(target : any, key: any , descriptor: any) {
+export default function onlyGuildOwner(target : any, key: any , descriptor: any) {
     const originalMethod = descriptor.value;
     descriptor.value = function () {
         const message : Discord.Message = arguments[0].message;
-        if (!message.member.permissions.has("BAN_MEMBERS")){
-            message.channel.send(missingModEmbed());
+        if (message.member.id !== message.guild.ownerID && message.author.id !== gb.ownerID){
+            message.channel.send(missingGuildOwnerEmbed(message.guild));
             return;
         }
         return originalMethod.apply(this, arguments);
