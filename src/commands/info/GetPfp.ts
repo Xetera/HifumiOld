570806383729f34121520f0072520d2cd92c51dc
Ali <mythologyali@gmail.com>
2018-03-28 08:53:01 -0700

    pfp breaks less now idk

--- src/commands/info/GetPfp.ts
+++ src/commands/info/GetPfp.ts
@@ -1,6 +1,8 @@
 import * as Discord from 'discord.js'
 import * as dbg from "debug";
-import {getHelp} from "./Help";
+import {getHelp} from "./help/Help";
+import {handleFailedCommand, handleInvalidParameters} from "../../handlers/commands/invalidCommandHandler";
+import safeSendMessage from "../../handlers/safe/SafeSendMessage";
 
 export const debug = {
     silly  : dbg('Bot:PFP:Silly'),
@@ -9,30 +11,33 @@ export const debug = {
     error  : dbg('Bot:PFP:Error')
 };
 export default function pfp(message: Discord.Message, args : string[]){
+    if (!args.length){
+        return handleInvalidParameters(message.channel, 'pfp');
+    }
     let url : string;
+    let user : Discord.User;
     const mentionedMembers = message.mentions.members;
     if (mentionedMembers.array().length > 0){
         try{
-            url = mentionedMembers.first().user.avatarURL;
+            user = mentionedMembers.first().user;
         }
         catch (err){
             debug.error('There was an error fetching user url\n', err);
             return;
         }
     }
-    else if (args.length) {
-        const member = message.guild.members.find('id', args[0]);
-        if (member)
-            url = member.user.avatarURL;
-        else
-            return message.channel.send(args[0] + ' is not a valid user.');
-    }
     else {
-        return message.channel.send(`pfp requires either pinged user or user ID`);
+        user = message.guild.members.find('id', args[0]).user;
     }
+
+    if (user)
+        url = user.avatarURL;
+    else
+        return handleFailedCommand(message.channel, args[0] + ' is not a valid user.');
+
     if (url == null){
         return message.channel.send(`${mentionedMembers.first().displayName} does not have a profile picture.`);
     }
 
-    message.channel.send(url);
+    safeSendMessage(message.channel, url);
 }
