494c3f7907470456b7778386a39cec3d9e3c4de7
Ali <mythologyali@gmail.com>
2018-03-31 21:32:37 -0700

    refactored alexa

--- src/API/Alexa.ts
+++ src/API/Alexa.ts
@@ -30,41 +30,42 @@ export class Alexa {
 
     public checkMessage(message : Discord.Message, bot :Discord.Client) : void {
         let alexaRequest = message.content.match(/alexa/i);
-        if (message.system) return;
-
-        else if (gb.instance.database.getIgnored(message.member) === true)
+        if (message.system || gb.instance.database.getIgnored(message.member) === true )
             return;
 
-        if (message.channel instanceof Discord.TextChannel){
-            if (message.channel.name === 'chat-with-alexa' || alexaRequest || message.isMentioned(bot.user)) {
-                // don't respond to messages not meant for me
-                if (message.mentions.users.array().length !== 0 && !message.isMentioned(bot.user)) return;
-                else if (message.content.startsWith('-')) return;
+        else if (!(message.channel instanceof Discord.TextChannel)) {
+            return;
+        }
+        else if (message.channel.name === 'chat-with-alexa' || alexaRequest || message.isMentioned(bot.user)) {
+            // don't respond to messages not meant for me
+            if (message.mentions.users.array().length !== 0 && !message.isMentioned(bot.user))
+                return;
+            else if (message.content.startsWith('-') || message.content.startsWith(gb.instance.database.getPrefix(message.guild.id)))
+                return;
 
-                debug.info(`${message.member.nickname || message.author.username} from guild ${message.member.guild} mentioned me in ${message.channel.name}`, "Alexa");
-                message.react('👀');
+            debug.info(
+                `[${message.member.guild}]::${message.channel.name}::<${message.author.username}> cleverbot call`, "Alexa");
+            message.react('👀');
 
-                if (message.channel.name === 'chat-with-alexa')
-                    this.say(message.content, false).then((resp : string) => {
-                    message.channel.send(resp);
-                });
-                else
-                    this.say(message.content).then((resp : string) => {
-                    message.channel.send(resp);
-                });
-            }
+            if (message.channel.name === 'chat-with-alexa')
+                this.say(message.content, false).then((resp : string) => {
+                message.channel.send(resp)
+            });
+            else
+                this.say(message.content).then((resp : string) => {
+                message.channel.send(resp);
+            });
         }
     }
 
     public say(phrase : string, replaceKeyword : boolean = true) : Promise<string>{
-        const that = this;
-        return new Promise<string>(function (resolve, reject) {
+        return new Promise<string>( (resolve, reject) => {
             let parsedArg :string;
             if (replaceKeyword)
-                parsedArg = that.replaceKeyword(phrase);
+                parsedArg = this.replaceKeyword(phrase);
             else
                 parsedArg = phrase;
-            that.cleverbot.say(parsedArg).then((response : string) => {
+            this.cleverbot.say(parsedArg).then((response : string) => {
                 resolve(response);
             }).catch(err => {
                 reject(err);
