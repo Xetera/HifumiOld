26b937a4b2b0c363d2c1e5d7d99a92c652d018f5
Ali <mythologyali@gmail.com>
2018-04-08 14:44:39 -0700

    fixed logging embeds

--- src/embeds/logging/logBanEmbed.ts
+++ src/embeds/logging/logBanEmbed.ts
@@ -1,12 +1,13 @@
 import {GuildMember, RichEmbed, User} from "discord.js";
 import {Offense} from "../../moderation/interfaces";
 
-export default function logBanEmbed(user: User, reason: string | Offense, soft?: boolean): RichEmbed{
+export default function logBanEmbed(user: User, reason: string | Offense, bannedBy: User): RichEmbed{
     return new RichEmbed()
         .setTitle(`User Banned ⛔`)
         .setThumbnail(user.avatarURL)
         .setDescription(user)
         .setColor('#ff0000')
         .addField(`Reason`, reason ? reason : 'No reason given')
+        .addField(`Banned By`, bannedBy)
         .setTimestamp()
 }
--- src/embeds/logging/logMuteEmbed.ts
+++ src/embeds/logging/logMuteEmbed.ts
@@ -1,15 +1,16 @@
-import {Channel, GuildMember, RichEmbed, TextChannel} from "discord.js";
+import {GuildMember, RichEmbed, TextChannel} from "discord.js";
 import {getMuteTime} from "../../utility/Settings";
 import {formattedTimeString, formatTime} from "../../utility/Util";
 import {Offense} from "../../moderation/interfaces";
 
-export default function logMutedEmbed(member: GuildMember): RichEmbed {
+export default function logMutedEmbed(member: GuildMember, mutedBy: GuildMember, reason: string | Offense, duration: number): RichEmbed {
     return new RichEmbed()
         .setTitle(`User Muted 🔇`)
         .setThumbnail(member.user.avatarURL)
         .setDescription(member)
         .setColor('#ffa739')
-        .addField(`Reason`, `Spamming`, true)
-        .addField(`Duration`, formattedTimeString(getMuteTime()), true)
+        .addField(`Muted By`, mutedBy)
+        .addField(`Reason`, reason, true)
+        .addField(`Duration`, formattedTimeString(duration ? duration : getMuteTime()), true)
         .setTimestamp();
 }
