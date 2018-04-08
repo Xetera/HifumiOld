9fcc88655cf662db20582d20e28d887393e271e8
Ali <mythologyali@gmail.com>
2018-04-08 14:43:48 -0700

    event handling updated

--- src/events/onChannelCreate.ts
+++ src/events/onChannelCreate.ts
@@ -1,26 +1,6 @@
-import * as Discord from 'discord.js'
-import * as dbg from "debug";
-import {log} from "../utility/Logging";
+import {LogManager} from "../handlers/logging/logManager";
+import {Channel} from "discord.js";
 
-export const debug = {
-    silly  : dbg('Bot:onChannelCreate:Silly'),
-    info   : dbg('Bot:onChannelCreate:Info'),
-    warning: dbg('Bot:onChannelCreate:Warning'),
-    error  : dbg('Bot:onChannelCreate:Error')
-};
-
-export default function onChannelCreate(channel : Discord.Channel){
-    if (channel instanceof Discord.TextChannel || channel instanceof Discord.VoiceChannel){
-        const logsChannel : Discord.Channel = channel.guild.channels.find('name', 'logs');
-        if (!logsChannel || logsChannel === undefined)
-            return debug.info(`A new channel was created in ${channel.guild} but a logs channel was not found.`, "onChannelCreate");
-        let creator : string;
-        channel.guild.fetchAuditLogs().then(audit => {
-            creator = audit.entries.first().executor.username;
-
-            log(channel.guild,
-                `${creator} created a new ${channel.type} channel '${channel.name}'`)
-
-        });
-    }
+export default function onChannelCreate(channel: Channel){
+    LogManager.logChannelCreate(channel);
 }
