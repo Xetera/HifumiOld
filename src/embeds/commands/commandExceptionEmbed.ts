df168ccd155254a567e68e705f07b1c94a054565
Ali <mythologyali@gmail.com>
2018-04-09 19:32:39 -0700

    snarky response when users are trying to get alexa to mention everyone

--- src/embeds/commands/commandExceptionEmbed.ts
+++ src/embeds/commands/commandExceptionEmbed.ts
@@ -3,10 +3,14 @@ import {Channel, RichEmbed, TextChannel} from "discord.js";
 export function handleFailedCommand(channel: Channel, message : string){
     // we don't want @everyone pings going off because of this
     const out = message.replace('@', '\`@\`');
+
     let embed = new RichEmbed()
         .setColor('#ff1513')
-        .setTitle(` ❌ Something went wrong`)
+        .setTitle(`Something went wrong`)
         .setDescription(out);
+    if (message !== out){
+        embed.setFooter(`Nice try btw`)
+    }
     if (channel instanceof TextChannel)
         channel.send(embed);
 }
