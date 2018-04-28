98bc7190d735394e44ea49478def827338f7e976
Ali <mythologyali@gmail.com>
2018-04-28 16:32:51 -0700

    changed help configurations

--- src/API/Cleverbot.ts
+++ src/API/Cleverbot.ts
@@ -64,7 +64,7 @@ export class Cleverbot {
                 return;
             else if (message.content.startsWith('-') || message.content.startsWith(await gb.instance.database.getPrefix(message.guild.id)))
                 return;
-            
+
             else if (this.isRateLimited(message.member.id, message)){
                 return;
             }
