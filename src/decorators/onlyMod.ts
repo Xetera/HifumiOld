0add2baca39ebf58d4bcf8c776513d64ff65d9f3
Ali <mythologyali@gmail.com>
2018-04-28 16:55:19 -0700

    owner overrides for mod only commands

--- src/decorators/onlyMod.ts
+++ src/decorators/onlyMod.ts
@@ -1,11 +1,12 @@
 import * as Discord from "discord.js";
 import missingModEmbed from "../embeds/permissions/missingModEmbed";
+import gb from "../misc/Globals";
 
 export default function onlyMod(target : any, key: any, descriptor: any) {
     const original = descriptor.value;
     descriptor.value = function () {
         const message : Discord.Message = arguments[0].message;
-        if (!message.member.permissions.has("BAN_MEMBERS")){
+        if (!message.member.permissions.has("BAN_MEMBERS") || message.member.id !== gb.ownerID){
             return missingModEmbed(message.guild).then(e => {
                 return void message.channel.send(e);
             });
