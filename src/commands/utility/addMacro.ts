a26ba33905f85dfdc7b5af60b3ba592863c4394d
Ali <mythologyali@gmail.com>
2018-05-05 04:23:16 -0700

    addmacro no longer allows mentions

--- src/commands/utility/addMacro.ts
+++ src/commands/utility/addMacro.ts
@@ -14,18 +14,22 @@ export default async function addMacro(message: Message, input: [string, string]
             message.channel, `${gb.emojis.get('alexa_boi')} How do you expect me to remember all that? Try something shorter.`
         );
     }
-
-    if (help.commands.map(command => command.name).includes(macroName)) {
+    else if (help.commands.map(command => command.name).includes(macroName)) {
         return void handleFailedCommand(
             message.channel, "That macro is already a command name, try to pick something else."
         );
     }
+    else if (message.mentions.users.size){
+        return void handleFailedCommand(
+            message.channel,
+            `Mentions are not allowed in macros, do you know how annoying it is to get pinged by bots? (⁎˃ᆺ˂)`
+        )
+    }
 
-    // @ts-ignore
     gb.instance.database.getMacroCount(message.guild.id).then(async (count: number) => {
         if (count >= 50) {
             return void handleFailedCommand(
-                message.channel, "Whoa, you already have 50 macros saved, you'll have to delete some first "
+                message.channel, "Whoa, you already have 50 macros saved, time to delete some"
             );
         }
 
