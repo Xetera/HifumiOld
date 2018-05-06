414017cacec76ed8d9eaa716a60b012b98614dfc
Ali <mythologyali@gmail.com>
2018-05-06 04:02:16 -0700

    rerolled breaking change

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 worker: node src/index.js
-release: npm run migrate
--- src/embeds/commands/inviteEmbed.ts
+++ src/embeds/commands/inviteEmbed.ts
@@ -7,8 +7,8 @@ export default function inviteEmbed(){
     return new RichEmbed({
         title: 'Invites',
         description:
-        '[Support Server](https://discord.gg/RM6KUrf) - Get help here\n' +
-        '[Invite Me](https://discordapp.com/oauth2/authorize?client_id=372615866652557312&scope=bot&permissions=268463300) - Add me to your server',
+        '[Support Server](https://discord.gg/RM6KUrf)\n' +
+        '[Invite Me](https://discordapp.com/oauth2/authorize?client_id=372615866652557312&scope=bot&permissions=268463300)',
         footer: {
             text: 'Hifumi v' + hifumi.version,
             icon_url: 'https://cdn.discordapp.com/avatars/372615866652557312/c106f4dcedac9acf574381d78cf55313.png?size=2048'
