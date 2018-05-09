9b5fb889c422205b856b878bedbdd846a75e771a
Ali <mythologyali@gmail.com>
2018-05-09 02:07:06 -0700

    suggestions now require ban permissions to bypass checks

--- src/commands/suggestions/suggest.ts
+++ src/commands/suggestions/suggest.ts
@@ -9,13 +9,13 @@ export default async function suggest(message: Message, input: [string]){
     const [suggestion] = input;
 
     gb.instance.database.addSuggestion(message, suggestion).then(async(r: Partial<Suggestion>) => {
-        if (message.member.hasPermission('ADMINISTRATOR')){
+        if (message.member.hasPermission('BAN_MEMBERS')){
             safeSendMessage(message.channel, `Alright sir, I'll get that straight past the review stage for you.`);
             approveSuggestion(message, [r.suggestion_id!])
         }
         else {
-            safeSendMessage(message.channel, `Alright, I added your suggestion. Your suggestion will be listed once a moderator approves it.`)
+            safeSendMessage(message.channel, `Alright, I added your suggestion. It will be listed once a moderator approves it.`);
             LogManager.logNewSuggestion(message.member);
         }
-    })
+    });
 }
