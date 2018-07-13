b944af647416ca842df1d603cd6eaaf003ca0adb
Unknown <mythologyali@gmail.com>
2018-07-13 06:22:59 -0700

    $avatar now shows reverse image search results

--- src/embeds/commands/pfpEmbed.ts
+++ src/embeds/commands/pfpEmbed.ts
@@ -3,7 +3,7 @@ import {GuildMember, RichEmbed} from "discord.js";
 export default function pfpEmbed(member: GuildMember, found: boolean = true){
     return new RichEmbed()
         .setTitle(`${member.user.username}'s profile picture`)
-        .setDescription(`[Link to the picture](${member.user.avatarURL})`)
-        .setImage(member.user.avatarURL)
+        .setDescription(`[Image URL](${member.user.avatarURL})\n[Reverse search results](https://www.google.com/searchbyimage?&image_url=${member.user.displayAvatarURL})`)
+        .setImage(member.user.displayAvatarURL)
         .setColor(member.highestRole.color)
 }
