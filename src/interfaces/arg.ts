c3961ab37b28143074a8bf8ae3f85d91351cfc37
Unknown <mythologyali@gmail.com>
2018-07-26 04:43:10 -0700

    dropped inversify

---
+++ src/interfaces/arg.ts
@@ -0,0 +1,25 @@
+import {register} from "../handlers/commands/registry";
+
+export enum ArgType {
+    String = 'string',
+    Number = 'number',
+    Member = 'member',
+    Message = 'message',
+    Boolean = 'boolean',
+    Channel = 'channel',
+    None = 'none'
+}
+
+export interface ArgOptions {
+    type: ArgType;
+    options?: {
+        minRange?: number;
+        maxRange?: number;
+        optional?: boolean;
+        maxLength?: number;
+        channelType?: AllChannelTypes;
+        minWords?: number;
+        raw?: boolean;
+        strict?: boolean;
+    }
+}
