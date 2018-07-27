dd8c8dbf1b040e7fe0dbe82f4d013a4828424747
Unknown <mythologyali@gmail.com>
2018-07-26 18:38:42 -0700

    Moved Client into the ioc container

---
+++ src/interfaces/injectables/client.interface.ts
@@ -0,0 +1,5 @@
+import {Client} from 'discord.js'
+
+export const IClient = Client;
+export abstract class DiscordClient extends IClient {};
+
--- src/misc/ioc.config.ts
+++ src/misc/ioc.config.ts
@@ -5,11 +5,16 @@ import {ICleverbot} from "../interfaces/injectables/cleverbot.interface";
 import {Cleverbot} from "../API/Cleverbot";
 import {ICommandHandler} from "../interfaces/injectables/commandHandler.interface";
 import CommandHandler from "../handlers/commands/CommandHandler";
+import {DiscordClient, IClient} from "../interfaces/injectables/client.interface";
 
 export function setupContainers(){
     Container.bind(ITokenBucket).to(TokenBucket).scope(Scope.Singleton);
     Container.bind(ICleverbot).to(Cleverbot).scope(Scope.Singleton);
     Container.bind(ICommandHandler).to(CommandHandler).scope(Scope.Singleton);
+    // a bit of a hack, we need a single instance of the discord client
+    // to exist but we can't make it extend an abstract class easily
+    // since it's not our own class
+    Container.bind(IClient).to(DiscordClient).scope(Scope.Singleton);
 
     // We need to reference these once so that the singleton
     // classes are loaded before the application starts
