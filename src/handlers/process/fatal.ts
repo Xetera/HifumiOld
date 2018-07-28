63359ba520daf66da97b91c3ec9d4a46e115f31f
Unknown <mythologyali@gmail.com>
2018-07-28 00:42:37 -0700

    fatal added

---
+++ src/handlers/process/fatal.ts
@@ -0,0 +1,10 @@
+import gb from "../../misc/Globals";
+import {debug} from "../../utility/Logging";
+
+export function handleFatalErrorGracefully(err: Error){
+    debug.error(`Received fatal error signal from a module, switching to sleep mode.`);
+    debug.error(err);
+    const bot = gb.instance.bot;
+    bot.user.setActivity('Sleeping...');
+    gb.sleeping = true;
+}
