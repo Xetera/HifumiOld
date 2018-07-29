ed7cdfc2bce891f6514658aabd2091881b989c24
Unknown <mythologyali@gmail.com>
2018-07-28 21:09:09 -0700

    logging changed to winston

--- src/utility/Logging.ts
+++ src/utility/Logging.ts
@@ -1,25 +1,46 @@
 import * as Discord from'discord.js'
 import {gb} from "../misc/Globals";
+import * as winston from "winston";
 const cli = require('heroku-cli-util');
 
-export let debug = {
-    silly  : function(obj : any, identifier:string = ''){
-        if (identifier !== '') identifier += ':';
-        console.log(identifier + cli.color.cyan(obj))
+const options = {
+    file: {
+        level: 'info',
+        filename: `/logs/app.log`,
+        handleExceptions: true,
+        json: true,
+        maxsize: 5242880, // 5MB
+        maxFiles: 5,
+        colorize: false,
     },
-    info   : function(obj : any,identifier:string = ''){
-        if (identifier !== '') identifier += ':';
-        console.log(identifier +cli.color.green(obj))
+    console: {
+        level: 'debug',
+        handleExceptions: true,
+        json: false,
+        colorize: true,
+        format: winston.format.combine(
+            winston.format.colorize(),
+            winston.format.align(),
+            winston.format.timestamp(),
+            winston.format.printf((info) => {
+                const {
+                    timestamp, level, message, ...args
+                } = info;
+
+                const ts = timestamp.slice(0, 19).replace('T', ' ');
+                return `${ts} [${level}]: ${message} ${Object.keys(args).length ? JSON.stringify(args, null, 2) : ''}`;
+            }),
+        )
     },
-    warning: function(obj : any,identifier:string = ''){
-        if (identifier !== '') identifier += ':';
-        console.log(identifier + cli.color.yellow(obj))
-    },
-    error  : function(obj : any,identifier:string = ''){
-        if (identifier !== '') identifier += ':';
-        console.log(identifier + cli.color.red(obj))
-    }
 };
+export const debug = winston.createLogger({
+    transports: [
+        new winston.transports.File(options.file),
+
+        new winston.transports.Console(options.console)
+    ],
+    exitOnError: false,
+});
 
 interface GuildStats {
     name: string;
@@ -44,10 +65,10 @@ export function log(guild: Discord.Guild, message : string) : void {
     message = message.replace('@', '@');
     const logsChannel = gb.database.getLogsChannel(guild.id);
     if (!logsChannel){
-        return debug.info(`Tried to log a message in ${guild.name} but a logs channel was not found.`);
+        return void debug.info(`Tried to log a message in ${guild.name} but a logs channel was not found.`);
     }
    if (logsChannel instanceof Discord.TextChannel){
         logsChannel.send(message);
-        return debug.info(`Logged a message in ${guild.name}`);
+        return void debug.info(`Logged a message in ${guild.name}`);
    }
 }
