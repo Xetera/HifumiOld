40ddab332a1a1ac5bd9e04772f05ffd53418bf76
Unknown <mythologyali@gmail.com>
2018-07-29 04:24:59 -0700

    new log types for datadog

--- src/handlers/logging/datadog.ts
+++ src/handlers/logging/datadog.ts
@@ -1,11 +1,21 @@
-import { StatsD, Tags } from 'hot-shots'
-import {Environments} from "../../events/systemStartup";
+import {Tags} from 'hot-shots'
+// import {Environments} from "../../events/systemStartup";
 import {gb} from "../../misc/Globals";
+import {debug} from "../../utility/Logging";
 
-const datadog = new StatsD();
 
-export function increment(target: string, tags?: Tags){
-    if (gb.ENV === Environments.Production){
-        datadog.increment(target, tags);
-    }
+export function incrementStat(target: string, tags?: Tags) {
+    gb.stats.increment(target, tags);
+    debug.silly(`Incremented ${target}`);
 }
+
+export function distributionStat(target: string, value: number){
+    gb.stats.distribution(target, value);
+    debug.silly(`Recorded distribution for ${target}`);
+}
+
+export function timedStat(target: string, value: number){
+    gb.stats.timing(target, value);
+    debug.silly(`Recorded timer for ${target}`);
+}
+
