129a846516a83f93675477843c3d019546a65c1e
Unknown <mythologyali@gmail.com>
2018-08-17 15:56:06 -0700

    Checking current branch name for push script
    updates to bash scripts
    
    
    webhook name

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -1,13 +1,76 @@
 #!/usr/bin/env bash
 
+
+error="\e[31m[ERROR]   \e[0m"
+success="\e[32m[SUCCESS] \e[0m"
+info="\e[36m[INFO]    \e[0m"
+
+send_webhook () {
+    
+    # Sends the event hook based on the event received
+    # [Must be passed with a \${} input]
+    
+    # $1 Event name ( Error | Success )
+    # $2 Title ( string of any length )
+    # $2 Description ( string of any length )
+    
+    local eval event="$1"
+    local eval title="$2"
+    local eval description="$3"
+    
+    local username=${username:-"Digital Ocean Webhook Worker"}
+    local pass_color=3066993
+    local fail_color=15158332
+    
+    local timestamp=$(date --utc +%FT%TZ)
+    local avatar=${avatar:-"https://peachsalmanac.com/wp-content/uploads/2017/08/Hifumi-new-game-768x614.jpg"}
+    
+    if [[ -z "$WEBHOOK_URL" ]]; then
+        echo -e "$error 'WEBHOOK_URL' env variable was not set, cannot notify discord server."
+        return 1
+    fi
+    
+    if [[ "$event" == "Error" ]]; then
+        echo -e "$info The request is failing"
+        local color=$fail_color
+        local embed_icon="https://cdn.discordapp.com/emojis/312314733816709120.png?"
+    else
+        echo -e "$info The request is passing"
+        local color=$pass_color
+        local embed_icon="https://cdn.discordapp.com/emojis/312314752711786497.png?"
+    fi
+    
+    local data='{
+        "username": "'"$username"'",
+        "avatar_url": "'"https://peachsalmanac.com/wp-content/uploads/2017/08/Hifumi-new-game-768x614.jpg"'",
+        "embeds": [ {
+                "color": '$color',
+                "author": {
+                    "name": "'"Hifumi Backend"'",
+                    "icon_url": "'"$embed_icon"'"
+                },
+                "title": "'"$title"'",
+                "description": "'"$description"'",
+                "timestamp": "'"$timestamp"'"
+        } ]
+    }'
+    
+    (curl --fail --progress-bar -A "Hifumi-API-Webhook" -H Content-Type:application/json -H X-Author:Xetera#9596 -d "$data" "$WEBHOOK_URL" \
+    && echo -e "$info Successfully sent webhook request.") \
+    || echo -e "$error Unable to send webhook request."
+}
+
+echo -e "$info Received request to post a webhook."
+echo -e "$info args: $@"
+
+# name=$(hostname)
+# if [[ name != "$required_hostname" ]]; then
+#     echo -e "$error Host was expected to be 'hifumi' but is instead '$name"
+#     exit 1
+# fi
+
 required_hostname=${required_hostname:-"hifumi"}
-
-error="\e[31m[ERROR]\e[0m"
-success="\e[32m[SUCCESS]\e[0m"
-info="\e[32m[INFO]\e[0m"
-
-name=$(hostname)
-if [[ name != $required_hostname ]]; then 
-    echo -e "$error Host was expected to be 'hifumi' but is instead '$name"
-    exit 1
-fi
+event="Error"
+title="Testing"
+description="xoxo -Sent from my server side bash script"
+send_webhook "\${event}" "\${title}" "\${description}"
--- src/scripts/push.sh
+++ src/scripts/push.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
-error="\e[31m[ERROR]\e[0m"
-success="\e[32m[SUCCESS]\e[0m"
+error="\e[31m[ERROR]   \e[0m"
+success="\e[32m[SUCCESS] \e[0m"
+info="\e[36m[INFO]    \e[0m"
 
 user=${1:-"Xetera"}
 repo=${2:-"Hifumi"}
-branch=${branch:-"staging"}
+checkout_from=${branch:-"staging"}
 
-project_name=$(basename $(git rev-parse --show-toplevel))
+project_name=$( basename $( git rev-parse --show-toplevel ) )
 
+# Must be in the right git repository for this to work
 if [[ $project_name != $repo ]]; then
     if [[ -n "$project_name" ]]; then
         echo "$error Not in a project repository"
@@ -18,15 +20,37 @@ if [[ $project_name != $repo ]]; then
     exit 1
 fi
 
-result=$( curl -s "https://api.travis-ci.org/$user/$repo.svg?branch=$branch" )
+echo -e "$info Checking the latest test on branch '$checkout_from'..."
 
-data=$( grep "pass" <<< $result )
+result=$( curl -s "https://api.travis-ci.org/$user/$repo.svg?branch=$checkout_from" )
 
+data=$( grep "passing" <<< $result )
+
+# checking if passing is found inside the html
+# I don't know if there's a better way to do this
+# but this seems to work fine for now
 if [[ -z "$data" ]]; then
-    echo -e "$error Repo in $user/$repo:$branch has not passed tests, exiting deployment..."
+    echo -e "$error Repo in $user/$repo:$checkout_from has not passed tests, exiting deployment!"
     exit 1
 else
-    echo -e "$success Repo is passing tests!"
+    echo -e "$info Target branch '$checkout_from' has passed the latest tests."
 fi
 
-resp=$( git branch )
+# getting the current branch name from git
+current_branch=$( git rev-parse --abbrev-ref HEAD )
+target_branch="master"
+
+if [[ $current_branch != $target_branch ]]; then
+    echo -e "$info Current branch is not $target_branch, attempting to checkout to master..."
+
+    checkout={ git checkout master } &> /dev/null
+    errored=$( grep "error" <<< $checkout )
+
+    if [[ -z $errored ]]; then
+        echo -e "$error Could not checkout to $target_branch branch, exiting deployment!"
+        exit 1
+    fi
+
+fi
+
+echo $current_branch
