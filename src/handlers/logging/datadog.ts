a1af481d42198e4fb5d956b08c0b52b34b8ed439
Xetera <mythologyali@gmail.com>
2018-11-16 13:32:52 -0800

    Revert "new log types for datadog"
    This reverts commit 40ddab332a1a1ac5bd9e04772f05ffd53418bf76.

--- src/handlers/logging/datadog.ts
+++ src/handlers/logging/datadog.ts
@@ -1,21 +1,11 @@
-import {Tags} from 'hot-shots'
-// import {Environments} from "../../events/systemStartup";
+import { StatsD, Tags } from 'hot-shots'
+import {Environments} from "../../events/systemStartup";
 import {gb} from "../../misc/Globals";
-import {debug} from "../../utility/Logging";
 
+const datadog = new StatsD();
 
-export function incrementStat(target: string, tags?: Tags) {
-    gb.stats.increment(target, tags);
-    debug.silly(`Incremented ${target}`);
+export function increment(target: string, tags?: Tags){
+    if (gb.ENV === Environments.Production){
+        datadog.increment(target, tags);
+    }
 }
-
-export function distributionStat(target: string, value: number){
-    gb.stats.distribution(target, value);
-    debug.silly(`Recorded distribution for ${target}`);
-}
-
-export function timedStat(target: string, value: number){
-    gb.stats.timing(target, value);
-    debug.silly(`Recorded timer for ${target}`);
-}
-
